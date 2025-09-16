15718130c17733b5e4b4d5f716702717f0751a80
Ved Dudhat <veddudhat18@gmail.com>
2025-09-16 14:59:51 +0530

    resume_report.py

--- resume_report.py
+++ resume_report.py
@@ -5,7 +5,7 @@ warnings.filterwarnings("ignore")
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
-    api_key="AIzaSyDQq69AxBRoHywVGL9cQ8bn7R4bbUbhVNE"
+    api_key=""
 )
 
 input_prompt = '''
